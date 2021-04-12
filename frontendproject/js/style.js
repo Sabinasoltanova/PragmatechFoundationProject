dd0a73607d669e214794cde751ba71a688da79eb
Soltanova Sabina <sabinasoltanova2000@gmail.com>
2021-04-12 22:48:53 +0400

    Added my project

---
+++ frontendproject/js/style.js
@@ -0,0 +1,23 @@
+$('.brand-active').owlCarousel({
+  loop:true,
+  nav:false,
+dots:false,
+autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      576:{
+          items:2
+      },
+      768:{
+          items:3
+      },
+      992:{
+          items:4
+      },
+      1200:{
+          items:5
+      }
+  }
+})
